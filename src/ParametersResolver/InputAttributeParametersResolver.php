b1fdb13190d506729ad81b14c67c1a6204b193f6
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-05-12 10:52:43 +0300

    Add `InputAttributeParametersResolver`

---
+++ src/ParametersResolver/InputAttributeParametersResolver.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Input\Http\ParametersResolver;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Yiisoft\Hydrator\NotResolvedException;
+use Yiisoft\Hydrator\ParameterAttributesHandler;
+use Yiisoft\Middleware\Dispatcher\ParametersResolverInterface;
+
+final class InputAttributeParametersResolver implements ParametersResolverInterface
+{
+    public function __construct(
+        private ParameterAttributesHandler $handler,
+    )
+    {
+    }
+
+    public function resolve(array $parameters, ServerRequestInterface $request): array
+    {
+        $result = [];
+
+        foreach ($parameters as $parameter) {
+            try {
+                $value = $this->handler->handle($parameter);
+            } catch (NotResolvedException) {
+                continue;
+            }
+
+            $result[$parameter->getName()] = $value;
+        }
+
+        return $result;
+    }
+}
