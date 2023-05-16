684fa5f877e7931375e65e21ae566ca8bc8d4831
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-05-16 10:01:38 +0300

    Add params

--- config/di-web.php
+++ config/di-web.php
@@ -8,11 +8,15 @@ use Yiisoft\Input\Http\Request\RequestProvider;
 use Yiisoft\Input\Http\Request\RequestProviderInterface;
 use Yiisoft\Input\Http\RequestInputParametersResolver;
 
+/** @var array $params */
+
 return [
     RequestProviderInterface::class => RequestProvider::class,
     RequestInputParametersResolver::class => [
         '__construct()' => [
             'hydrator' => Reference::to(ValidatingHydrator::class),
+            'throwInputValidationException' =>
+                $params['yiisoft/input-http']['requestInputParametersResolver']['throwInputValidationException'],
         ],
     ],
 ];
